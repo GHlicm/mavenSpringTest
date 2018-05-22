8ae74730818c8255a6b24b1297769d7b448008a8
licm <licm-b@licm-PC01.grandsoft.com.cn>
2018-05-22 14:35:02 +0800

    redis和mysql分布式锁

--- src/main/java/com/test/HelloSpringController.java
+++ src/main/java/com/test/HelloSpringController.java
@@ -1,12 +1,25 @@
 package com.test;
 
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mapper.DistributeLockMapper;
+import com.util.lockUtil.MysqlLock;
+
+
 @Controller
 @RequestMapping("/llo")
 public class HelloSpringController {
+	@Autowired
+	private MysqlLock mysqlLock;
 	
 	@ResponseBody
 	@RequestMapping("/123")
@@ -14,6 +27,20 @@ public class HelloSpringController {
 		System.out.println("xxxxxxxxxxxxxxxxxxxx");
 		A a = new A();
 		a.setS("string");
+		
+		mysqlLock.unlock();
+		
 		return "sssssssssssssss";
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Reader reader = Resources.getResourceAsReader("mybatis-conf.xml");
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			SqlSession session = sqlSessionFactory.openSession();
+			//TODO ...........突然想到用tomcat启动加载
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
