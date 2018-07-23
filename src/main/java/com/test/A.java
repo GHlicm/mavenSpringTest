6012e20836ab1741c300d0756bce0cbd141b7b27
licm <licm-b@licm-PC01.grandsoft.com.cn>
2018-07-23 09:56:57 +0800

    注释掉了main中的测试代码

--- src/main/java/com/test/A.java
+++ src/main/java/com/test/A.java
@@ -1,14 +1,31 @@
 package com.test;
 
+import java.util.concurrent.locks.Lock;
+
+import org.I0Itec.zkclient.ZkLock;
+
 public class A {
-	private String s;
-
-	public String getS() {
-		return s;
-	}
-
-	public void setS(String s) {
-		this.s = s;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+//		int count = 100;
+//		Lock lock = new ZkLock();
+//		
+//		Ticket ticket = new Ticket(count, lock);
+//		
+//		Thread t1 = new Thread(ticket, "窗口A");
+//		Thread t2 = new Thread(ticket, "窗口B");
+//		Thread t3 = new Thread(ticket, "窗口C");
+//		Thread t4 = new Thread(ticket, "窗口D");
+//		
+//		t1.start();
+//		t2.start();
+//		t3.start();
+//		t4.start();
+//		
+//		Thread.currentThread().join();
 	}
 	
 }
+
+
